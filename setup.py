c52932f7af6a6d7542b9726ef1fa96a3175d3b0d
Miguel Romero <romeromiguelin@gmail.com>
2021-08-18 16:58:51 -0500

    Plot functions added

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE', encoding='utf-8') as f:
 
 setup(
     name='nodeclass',
-    version='0.0.1',
+    version='0.0.2',
     description='Node classification package',
     long_description=readme,
     long_description_content_type='text/x-rst',
